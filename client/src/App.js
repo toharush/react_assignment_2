38137f6f8a98e0319cb830f2a9a3218935da4bc7
Toharush <toahr1010@gmail.com>
2022-12-05 13:11:06 +0200

    Added client with api to server

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { useState, useEffect } from "react";
+import ShoppingList from './components/ShoppingList/ShoppingList';
+
+function App() {
+
+  return (
+    <div className="App">
+      <ShoppingList />
+    </div>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const maionRouter = require('./src/routes/main');
 const dotenv = require('dotenv');
 const express = require('express');
+const cors = require('cors');
 const app = express(); 
 
 dotenv.config();
+
 require('./src/utils/db-client');
 
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use('/api/v1', maionRouter);
 
 app.listen(PORT, () => console.log(`App listen on port ${PORT}`));
